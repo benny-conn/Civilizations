2f35e807e73bf6691c0a98612458d21f7169fb20
Tolmikarc <benny@bennyc.io>
2021-01-24 23:41:28 -0800

    Fix Index Out of Bounds for List Command

--- src/main/java/net/tolmikarc/civilizations/command/ListCommand.kt
+++ src/main/java/net/tolmikarc/civilizations/command/ListCommand.kt
@@ -13,11 +13,11 @@ import org.mineacademy.fo.command.SimpleSubCommand
 class ListCommand(parent: SimpleCommandGroup?) : SimpleSubCommand(parent, "list") {
     override fun onCommand() {
         checkConsole()
-        val page = if (args.size > 1) args[1].toInt() else 1
+        val page = if (args.size > 1) args[0].toInt() else 1
         AsyncEnvironment.run {
             val lowerLimit = (page * 10) - 9
             val upperLimit = page * 10
-            tellNoPrefix("${Settings.PRIMARY_COLOR}======= ${Settings.SECONDARY_COLOR}Civs: ${args[0].capitalize()} ($page) ${Settings.PRIMARY_COLOR}=======")
+            tellNoPrefix("${Settings.PRIMARY_COLOR}======= ${Settings.SECONDARY_COLOR}Civs: ($page) ${Settings.PRIMARY_COLOR}=======")
             val civs = ArrayList(CivManager.all)
             for (i in lowerLimit..upperLimit) {
                 tellNoPrefix("${Settings.PRIMARY_COLOR} ${i}. ${civs[i - 1].name}")
